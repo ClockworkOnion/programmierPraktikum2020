73da5cab69822addbb6a7a2df89eb839227a5959
ClockWorkMini <49729188+mochiponzu@users.noreply.github.com>
2020-08-23 12:24:29 +0200

    Ein Hello World

--- src/Testclass.java
+++ src/Testclass.java
@@ -2,4 +2,11 @@ public class Testclass {
     // Noch garnichts
     // Jetzt etwas
     // Sieht man das?
+    // und das jetzt auch?
+    // Etwas fuer intellij
+    // Live share oder was??
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
