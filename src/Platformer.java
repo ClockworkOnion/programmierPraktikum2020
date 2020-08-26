22d910e59faf50255ed8f746921229fd55633b7c
ClockWorkMini <49729188+mochiponzu@users.noreply.github.com>
2020-08-26 18:09:19 +0200

    player ist da

--- src/Platformer.java
+++ src/Platformer.java
@@ -1,26 +1,28 @@
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-import java.io.File;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class Platformer extends JFrame {
 	private static final long serialVersionUID = 5736902251450559962L;
 
+	Player player;
+	// Graphics2D displayGraphics;
 	int xPos = 0;
 	int scrollSpeed = 10;
 	final int windowsSizeX = 1008;
 	final int windowsSizeY = 385;
+	BufferStrategy bufferStrategy;
 
 	BufferedImage levelImg;
 	public Platformer() {
+
+		player = new Player(new Vec2(50, 50));
 		//exit program when window is closed
 		this.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e){
@@ -29,13 +31,18 @@ public class Platformer extends JFrame {
 		});
 
 		levelImg = Level1.thisLevel;
+		// displayGraphics = (Graphics2D) levelImg.getGraphics();
 		this.setBounds(0, 0, windowsSizeX, windowsSizeY);
 		this.setVisible(true);
 
+		createBufferStrategy(2);
+		bufferStrategy = this.getBufferStrategy();
 
 		this.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
+
+				/*
 
 				if (e.getKeyCode() == KeyEvent.VK_LEFT && (xPos > 0 )) {
 					xPos -= scrollSpeed;
@@ -43,15 +50,49 @@ public class Platformer extends JFrame {
 				if (e.getKeyCode() == KeyEvent.VK_RIGHT  && (xPos < levelImg.getWidth() - windowsSizeX)){
 					xPos += scrollSpeed;
 				}
+				 */
+
+
+				if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
+					player.move(new Vec2(-10, 0));
+				}
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT ){
+					player.move(new Vec2(10, 0));
+				}
+
+
+
 				repaint();
 			}
 
 		});
 	}
 
+	private void draw(Graphics2D g2) {
+		// BufferedImage combinedImage = new BufferedImage(windowsSizeX, windowsSizeY, TYPE_INT_RGB);
+		// Graphics tempGraphics = combinedImage.getGraphics();
+		g2.drawImage(getVisibleLevel(), 0, 0, this);
+		g2.drawImage(player.getImage(), player.GetXPosition(), player.GetYPosition(), this);
+	}
+
+
+
+
 	@Override
 	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.drawImage(levelImg, 8 - xPos, 35, this);
+		Graphics2D g2 = null;
+		try {
+			g2 = (Graphics2D) bufferStrategy.getDrawGraphics();
+			draw(g2);
+		} finally {
+			if (g2 != null) { g2.dispose(); }
+		}
+		bufferStrategy.show();
+
+		// g.drawImage(combineGraphics(), 8 , 35, this);
+	}
+
+	private BufferedImage getVisibleLevel () {
+		return levelImg.getSubimage(xPos, 0, windowsSizeX-50, windowsSizeY-50);
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -5,12 +5,44 @@ import java.io.IOException;
 
 public class Player {
 
+    Vec2 position;
+    Vec2 lastPosition;
+    Vec2 velocity;
+    private int currentDisplaySprite = 0;
+
     // assets/Player/p3_walk/PNG/p3_walk01.png
     BufferedImage[] playerSprites = new BufferedImage[11];
+    BufferedImage mainSprite;
 
-    public Player () throws IOException {
-        for (int i = 0; i <= 10; i++ ) {
-            playerSprites[i] = ImageIO.read(new File("assets/Player/p3_walk/PNG/p3_walk01.png"))
+    public Player (Vec2 startPosition) {
+        try {
+            for (int i = 0; i <= 10; i++ ) {
+                playerSprites[i] = ImageIO.read(new File("assets/Player/p3_walk/PNG/p3_walk" + ((i < 9) ? ("0" + (i + 1)) : ("1" + (i - 9))) + ".png"));
+                // playerSprites[i] = ImageIO.read(new File("assets/Player/p3_walk/PNG/p3_walk0" + i+1 + ".png"));
+                mainSprite = ImageIO.read(new File("assets/Player/p3_walk/PNG/p3_walk01.png"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        position = startPosition;
+    }
+
+    public void move(Vec2 movement) {
+        position = position.add(movement);
+        currentDisplaySprite = (currentDisplaySprite + 1) % 11;
+    }
+
+    public BufferedImage getImage() {
+       return playerSprites[currentDisplaySprite];
+        // return mainSprite;
+    }
+
+    public int GetXPosition () {
+        return (int)position.x;
+    }
+
+    public int GetYPosition () {
+        return (int)position.y;
     }
 }
